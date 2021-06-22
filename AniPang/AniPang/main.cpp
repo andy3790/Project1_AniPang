e18e0e27638c27d32238e48b2ae70e9b74aab3cb
yyyyww <yyyyww@naver.com>
2021-06-23 01:02:14 +0900

    tset

--- AniPang/AniPang/main.cpp
+++ AniPang/AniPang/main.cpp
@@ -42,7 +42,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevinstance, LPSTR lpszCmdPa
 	}
 	return Message.wParam;
 }
-
+// test
 LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
 
 	PAINTSTRUCT ps;
